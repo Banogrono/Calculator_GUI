69f619d54e5180bea270072e2dd1324e76da5ea4
Banogrono <48155039+Banogrono@users.noreply.github.com>
2021-08-01 18:44:58 +0200

    - refactor code - change signatures of methods add, subtract, multiply, divide (Change operation logic issue)

--- src/Counting.java
+++ src/Counting.java
@@ -20,62 +20,44 @@ public class Counting {
         return result;
     }
 
-    static void add() {
-        double a = Double.parseDouble(boardContentP1);
-        double b = Double.parseDouble(boardContentP2);
+    static void add(double a, double b) {
         result = 0;
         result = a + b;
-
     }
 
-    static void subtract() {
-        double a = Double.parseDouble(boardContentP1);
-        double b = Double.parseDouble(boardContentP2);
+    static void subtract(double a, double b) {
         result = 0;
         result = a - b;
 
     }
 
-    static void multiply() {
-        double a = Double.parseDouble(boardContentP1);
-        double b = Double.parseDouble(boardContentP2);
+    static void multiply(double a, double b) {
         result = 0;
         result = a * b;
 
     }
 
-    static void divide() {
-        double a = Double.parseDouble(boardContentP1);
-        double b = Double.parseDouble(boardContentP2);
+    static void divide(double a, double b) {
+        if (a == 0 || b == 0) return;
         result = 0;
         result = a / b;
 
     }
 
     static void doMath() {
+        var a = Double.parseDouble(boardContentP1);
+        var b = Double.parseDouble(boardContentP2);
         switch (flag) {
-            case 0: { // Add
-                add();
-                break;
-            }
-            case 1: { // Sub
-                subtract();
-                break;
-            }
-            case 2: { // Mult
-                multiply();
-                break;
-            }
-            case 3: { // Div
-                divide();
-                break;
-            }
-            default: { // Wrong type
-                System.out.println("Wrong input");
-                break;
-            }
+            case 0 ->
+                    add(a, b);
+            case 1 ->
+                    subtract(a, b);
+            case 2 ->
+                    multiply(a, b);
+            case 3 ->
+                    divide(a, b);
+            default ->
+                    System.out.println("Wrong input");
         }
     }
-
-
 }
